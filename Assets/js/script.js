018256789e9455161300c8a619758f84af9f515d
Daniel John <dcampo85@gmail.com>
2024-01-07 00:17:45 -0500

    more variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,8 @@
 var owmAPI = "0ef465a588f6128c27d826989d773558";
 var currentCity = "";
 var lastCity = "";
+
+const getCurrentConditions = (event) => {
+    let city = $('search-city').val();
+    currentCity = $('search-city').val();
+}
