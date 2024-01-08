96147066014d6d215d2303ea139af0083e22210a
Daniel John <dcampo85@gmail.com>
2024-01-08 00:27:23 -0500

    js pull + display conditions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,8 +2,16 @@ var owmAPI = "0ef465a588f6128c27d826989d773558";
 var currentCity = "";
 var lastCity = "";
 
-const getCurrentConditions = (event) => {
-    let city = $('search-city').val();
+var showError = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+// this function pulls + displays current weather conditions
+var getCurrentConditions = (event) => {
+    let city = $('search-city').val(); // pulls city name from search box
     currentCity = $('search-city').val();
 
     let searchURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=" + owmAPI;
@@ -17,6 +25,8 @@ const getCurrentConditions = (event) => {
         saveCity(city);
         $('#search-error').text("");
 
-        
+        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weahter[0].icon + ".png";
+
+
     }
 }
